efe7f3d23ada3284556f15e0dce0f1c1bc2fb27a
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-15 11:46:57 -0300

    primeiros passos

---
+++ Projetos/exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,8 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Hello World 1!");
+        System.out.println("Hello World 2!");
+    }
+}
