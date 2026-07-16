e03f525135b0a59e1ed80ef5d38f3e36691821bd
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-16 14:41:13 -0300

    notação ponto

---
+++ Projetos/exercicios/src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class NotacaoPonto {
+    public static void main(String[] args) {
+        String s = "Bom dia X";
+        s = s.replace("X", "Pedro");
+        s = s.toUpperCase();
+        s = s.concat("!!\n");
+        System.out.println(s);
+
+        String x = "Ana\n".toUpperCase();
+        System.out.println(x);
+        System.out.println("Pedro\n".toUpperCase());
+
+        String y = "Bom dia X".replace("X", "Brasil\n");
+        System.out.println(y);
+
+        String z = "Bom dia Y".replace("Y", "Leticia").toUpperCase().concat("!!!");
+        System.out.println(z);
+    }
+}
