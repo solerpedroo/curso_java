2d7ad57784d0b90d8d61dc61a5a5c3074f4a77d3
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-19 10:55:21 -0300

    conversão tipos primitivos numéricos

---
+++ Projetos/exercicios/src/fundamentos/ConversaoTiposPrimitivosNumericos.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+public class ConversaoTiposPrimitivosNumericos {
+    public static void main(String[] args) {
+        double a = 1; // conversão implícita
+        System.out.println(a);
+
+        // solução padrão
+        // float b = 1.0F;
+
+        // cast
+        float b = (float) 1.123456; // explícita
+        System.out.println(b);
+
+        int c = 4;
+        byte d = (byte) c; // explícita (CAST)
+        System.out.println(d);
+
+        double e = 1;
+        int f = (int) e;
+        System.out.println(f);
+    }
+}
