0c053cbabab0323b01cf942d52188988291f00ce
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-21 19:40:42 -0300

    if

---
+++ Projetos/exercicios/src/controle/If.java
@@ -0,0 +1,23 @@
+package controle;
+
+import java.util.Scanner;
+
+public class If {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Informe a média: ");
+        double media = entrada.nextDouble();
+
+        if(media > 10 || media < 0){
+            System.out.println("Média inválida");
+        } else if(media >= 7){
+            System.out.println("Aprovado");
+        } else if(media >= 5){
+            System.out.println("Recuperação");
+        } else{
+            System.out.println("Reprovado");
+        }
+        entrada.close();
+    }
+}
