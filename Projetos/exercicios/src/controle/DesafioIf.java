6ab728d84ae81464594e2e68b754ca48a3687380
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-21 19:46:07 -0300

    atualizaçao desafio

--- Projetos/exercicios/src/controle/DesafioIf.java
+++ Projetos/exercicios/src/controle/DesafioIf.java
@@ -10,7 +10,11 @@ public class DesafioIf {
 
         /* RESPOSTA:
         * Código com incosistência, ele irá printar o "Fera" por conta do ";" após o if, mesmo a condição estando certa
-        * pois ele quebra o controle do if e identifca o System.out como um bloco de código
+        * pois ele quebra o controle do if e identifca o System.out como um bloco de código, o correto seria:
+        *
+        * if(nota>=9.0){
+            System.out.println("Fera");
+          }
         * */
     }
 }
