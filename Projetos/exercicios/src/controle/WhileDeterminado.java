822ec096ddb58ac6b9ca904a035290cfba0c48ce
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-22 12:43:29 -0300

    while

---
+++ Projetos/exercicios/src/controle/WhileDeterminado.java
@@ -0,0 +1,11 @@
+package controle;
+
+public class WhileDeterminado {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<10){
+            System.out.printf("i = %d\n", i);
+            i++;
+        }
+    }
+}
