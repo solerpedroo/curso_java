614a08fab8c848ef221e720c55af81ed478bcf09
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-24 20:36:51 -0300

    ajustes na ordem do código

--- Projetos/exercicios/src/classe/DataTeste.java
+++ Projetos/exercicios/src/classe/DataTeste.java
@@ -10,12 +10,12 @@ public class DataTeste {
         d1.mes = 10;
         d1.ano = 2006;
 
-        System.out.printf("Data 1:\n%d/%d/%d\n\n", d1.dia, d1.mes, d1.ano);
-
         Data d2 = new Data();
         d2.dia = 15;
         d2.mes = 7;
         d2.ano = 2010;
+
+        System.out.printf("Data 1:\n%d/%d/%d\n\n", d1.dia, d1.mes, d1.ano);
         System.out.printf("Data 2:\n%d/%d/%d", d2.dia, d2.mes, d2.ano);
     }
 }
