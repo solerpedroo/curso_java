850d13ee63490a0d3c2267c6e21290fcf241b4d3
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-27 15:56:26 -0300

    desafio membros de classe x instancias

--- Projetos/exercicios/src/classe/Produto.java
+++ Projetos/exercicios/src/classe/Produto.java
@@ -3,7 +3,7 @@ package classe;
 public class Produto {
     String nome;
     double preco;
-    double desconto;
+    static double desconto = 0.25;
 
     // construtor default
     Produto(){
@@ -11,10 +11,9 @@ public class Produto {
     }
 
     // construtor explícito
-    Produto(String nomeInicial, double precoInicial, double descontoInicial){
+    Produto(String nomeInicial, double precoInicial){
         nome = nomeInicial;
         preco = precoInicial;
-        desconto = descontoInicial;
     }
 
     // é possível ter métodos com nomes iguais porém com parâmetros diferentes
--- Projetos/exercicios/src/classe/ProdutoTeste.java
+++ Projetos/exercicios/src/classe/ProdutoTeste.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 
 public class ProdutoTeste {
     public static void main(String[] args) {
-        Produto p1 = new Produto("Notebook", 4356.89, 0.25);
+        Produto p1 = new Produto("Notebook", 4356.89);
 
         var p2 = new Produto();
         p2.nome = "Caneta Preta";
         p2.preco = 12.56;
-        p2.desconto = 0.29;
 
-        System.out.println(p1.nome);
-        System.out.println(p2.nome);
+        Produto.desconto = 0.50;
+
+        System.out.println(p1.nome + " " + p1.precoComDesconto(0));
+        System.out.println(p2.nome + " " +  p2.precoComDesconto(0));
 
         double precoFinal1 = p1.precoComDesconto(0);
         double precoFinal2 = p2.precoComDesconto(0.1);
