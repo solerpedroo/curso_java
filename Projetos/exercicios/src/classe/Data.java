6c4fef635e33cf9ccaa849a54bb692c3aee2abcf
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-27 13:01:02 -0300

    desafio construtores

--- Projetos/exercicios/src/classe/Data.java
+++ Projetos/exercicios/src/classe/Data.java
@@ -5,6 +5,18 @@ public class Data {
     int mes;
     int ano;
 
+    Data(){
+        dia = 1;
+        mes = 1;
+        ano = 1970;
+    }
+
+    Data(int diaInicial, int mesInicial, int anoInicial){
+        dia = diaInicial;
+        mes = mesInicial;
+        ano = anoInicial;
+    }
+
     String dataFormatada(){
         return String.format("%d/%d/%d", dia, mes, ano);
     }
--- Projetos/exercicios/src/classe/DataTeste.java
+++ Projetos/exercicios/src/classe/DataTeste.java
@@ -6,14 +6,9 @@ public class DataTeste {
         // criar duas instâncias de datas
 
         Data d1 = new Data();
-        d1.dia = 31;
-        d1.mes = 10;
         d1.ano = 2006;
 
-        Data d2 = new Data();
-        d2.dia = 15;
-        d2.mes = 7;
-        d2.ano = 2010;
+        Data d2 = new Data(15, 7, 2010);
 
         System.out.println(d1.dataFormatada());
         System.out.printf(d2.dataFormatada());
