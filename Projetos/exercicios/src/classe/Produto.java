9e4581011ed343bea00fa814455e822dca3f8f50
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-27 12:57:12 -0300

    construtores

--- Projetos/exercicios/src/classe/Produto.java
+++ Projetos/exercicios/src/classe/Produto.java
@@ -5,6 +5,18 @@ public class Produto {
     double preco;
     double desconto;
 
+    // construtor default
+    Produto(){
+
+    }
+
+    // construtor explícito
+    Produto(String nomeInicial, double precoInicial, double descontoInicial){
+        nome = nomeInicial;
+        preco = precoInicial;
+        desconto = descontoInicial;
+    }
+
     // é possível ter métodos com nomes iguais porém com parâmetros diferentes
     double precoComDesconto(double descontoDoGerente){
         return preco * (1 - (desconto + descontoDoGerente));
