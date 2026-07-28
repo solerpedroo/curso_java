f585a8a0d0c184256f9eb2d7cfa76ebb032938b4
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-28 10:05:59 -0300

    desafio

---
+++ Projetos/exercicios/src/classe/PrimeiroTrauma.java
@@ -0,0 +1,15 @@
+package classe;
+
+public class PrimeiroTrauma {
+
+    int a = 3;
+
+    static int b = 4; // 2ª opção
+
+    public static void main(String[] args) {
+        PrimeiroTrauma p = new PrimeiroTrauma(); // 1ª opção
+
+        System.out.println(p.a);
+        System.out.println(b);
+    }
+}
