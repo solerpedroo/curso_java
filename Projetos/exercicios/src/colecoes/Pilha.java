9ceda1a598381bd00494ddef4490f03bab0cc753
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-30 13:03:08 -0300

    pilha

---
+++ Projetos/exercicios/src/colecoes/Pilha.java
@@ -0,0 +1,26 @@
+package colecoes;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Pilha {
+    public static void main(String[] args) {
+        Deque<String> livros = new ArrayDeque<>();
+
+        livros.add("O pequeno príncipe"); // retorna um boolean
+        livros.push("Don Quixote");
+        livros.push("O Hobbit"); // lança uma exceção caso tamanho já alcançado
+
+        System.out.println(livros.peek());
+        System.out.println(livros.element());
+
+        System.out.println(livros.pop()); // joga uma exceção
+        System.out.println(livros.poll()); // retorna null
+        System.out.println(livros.poll());
+        System.out.println(livros.remove()); // joga exceção
+
+        // livros.size();
+        // livros.clear();
+        // livros.contains();
+    }
+}
