dfec062c8cfeb1277ff108f0f01d0dac98f7bd7d
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-07-30 10:17:17 -0300

    set homogeneo

---
+++ Projetos/exercicios/src/colecoes/ConjuntoOrganizado.java
@@ -0,0 +1,20 @@
+package colecoes;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ConjuntoOrganizado {
+    public static void main(String[] args) {
+        // Set<String > listaAprovados = new HashSet<>(); // desse lado pode ter ou não o identificador de tipo ou apenas o <>
+        TreeSet<String > listaAprovados = new TreeSet<>(); // ordem de inserção usa-se o TreeSet
+        listaAprovados.add("Pedro");
+        listaAprovados.add("Ana");
+        listaAprovados.add("Carlos");
+        listaAprovados.add("Lucas");
+
+        for (String candidato: listaAprovados){
+            System.out.println(candidato);
+        }
+    }
+}
