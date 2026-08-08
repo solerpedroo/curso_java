6152671c87943d2d275129070f29991df7a0758f
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-08-08 14:23:34 -0300

    herança 2

--- Projetos/exercicios/src/oo/heranca/Jogo.java
+++ Projetos/exercicios/src/oo/heranca/Jogo.java
@@ -2,29 +2,35 @@ package oo.heranca;
 
 public class Jogo {
     public static void main(String[] args) {
-        Monstro j1 = new Monstro();
-        j1.x = 10;
-        j1.y = 10;
+        Monstro monstro = new Monstro();
+        monstro.x = 10;
+        monstro.y = 10;
 
-        Heroi j2 = new Heroi();
-        j2.x = 10;
-        j2.y = 11;
+        Heroi heroi = new Heroi();
+        heroi.x = 10;
+        heroi.y = 11;
 
-        System.out.println(j1.vida);
-        System.out.println(j2.vida);
+        System.out.println(monstro.vida);
+        System.out.println(heroi.vida);
 
-        j1.atacar(j2);
-        j2.atacar(j1);
+        monstro.atacar(heroi);
+        heroi.atacar(monstro);
 
-        System.out.println(j1.vida);
-        System.out.println(j2.vida);
+        monstro.atacar(heroi);
+        heroi.atacar(monstro);
 
-        j1.andar(Direcao.NORTE);
-        j1.andar(Direcao.LESTE);
-        j1.andar(Direcao.NORTE);
-        j1.andar(Direcao.LESTE);
+        monstro.atacar(heroi);
+        heroi.atacar(monstro);
 
-        System.out.println(j1.x);
-        System.out.println(j1.y);
+        System.out.println("Monstro tem => " + monstro.vida);
+        System.out.println("Heroi tem => " +heroi.vida);
+
+        monstro.andar(Direcao.NORTE);
+        monstro.andar(Direcao.LESTE);
+        monstro.andar(Direcao.NORTE);
+        monstro.andar(Direcao.LESTE);
+
+        System.out.println(monstro.x);
+        System.out.println(monstro.y);
     }
 }
