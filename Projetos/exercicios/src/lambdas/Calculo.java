0779368849c938982ce91203dba73a001c9d0c7d
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-09-10 20:46:49 -0300

    functional interface

--- Projetos/exercicios/src/lambdas/Calculo.java
+++ Projetos/exercicios/src/lambdas/Calculo.java
@@ -1,5 +1,14 @@
 package lambdas;
 
+@FunctionalInterface
 public interface Calculo {
     public abstract double executar(double a, double b);
+
+    default String metodoDefault(){
+        return "não interfere na implementação";
+    }
+
+    static String metodoStatic(){
+        return "não interfere na implementação";
+    }
 }
